89205dcfac7ed202b7a0dfa94d5e62f05abd4c91
tandavala <tandavala@github.com>
2018-01-19 13:59:42 +0100

    cnfigurando o package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='funniest',
+        version='0.1',
+        description='The funniest joke in the world',
+        url='https://github.com/pyzi/funniest',
+        author='Jose Tandavala',
+        author_email='jose.tandavala@gmail.com',
+        license='MIT',
+        packages=['funniest'],
+        zip_safe=False)
