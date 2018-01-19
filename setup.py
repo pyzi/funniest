f010b65c86f210a3dd8b1fa6fc742f0e401badfb
tandavala <tandavala@github.com>
2018-01-19 14:07:41 +0100

    upading the dir

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 
-setup(name='funniest',
+setup(name='lunguka',
         version='0.1',
         description='The funniest joke in the world',
         url='https://github.com/pyzi/funniest',
         author='Jose Tandavala',
         author_email='jose.tandavala@gmail.com',
         license='MIT',
-        packages=['funniest'],
+        packages=['lunguka'],
         zip_safe=False)
